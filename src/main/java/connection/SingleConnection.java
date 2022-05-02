a417df92414269d934f4950f3a576b31592cc5f4
unknown <ggcodigos@gmail.com>
2022-05-02 10:39:25 -0300

    heroku setup

--- src/main/java/connection/SingleConnection.java
+++ src/main/java/connection/SingleConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 
 public class SingleConnection{
 
-	private static String url = "jdbc:postgresql://localhost:5432/projectjsp";
-	private static String user = "postgres";
-	private static String password = "root";
+	private static String url = "jdbc:postgresql://ec2-3-217-113-25.compute-1.amazonaws.com:5432/db6b89gha0b7m3";
+	private static String user = "lvzvcnwplwwzjo";
+	private static String password = "967d7893c2e08af80858ac594497a3b8bf1c775abf6b81388a67f57c2748c3c7";
 	private static Connection connection = null;
 
 	static {
